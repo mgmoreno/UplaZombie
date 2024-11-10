86f1caae3774fa2042048e0b93fe78879be4c0c1
mgmoreno <mgmorenopatino@gmail.com>
2024-11-10 16:55:43 -0300

    added ammo and reload logic/animation

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -10,24 +11,53 @@ public class Gun : MonoBehaviour
 
     [Header("Properties")]
     [SerializeField] private LayerMask fireMask;
-    [SerializeField] private int damage = 1;    
+    [SerializeField] private int damage = 1;
+    [SerializeField] private int clipSize = 6;
+
+    private int _currentAmmo;
+    private bool _reloading;
 
     private int AnimFire = Animator.StringToHash("Fire");
+    private int AnimReload = Animator.StringToHash("Reload");
+
+    private void Awake()
+    {
+        _currentAmmo = clipSize;
+    }
 
     private void Update()
     {
         animator.SetBool(AnimFire, false);
+        animator.SetBool(AnimReload, false);
+
+        if (_reloading) 
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            Reload();
+            return;
+        }
 
         if (Input.GetMouseButtonDown(0))
         {
+            if (_currentAmmo <= 0) 
+            {
+                Reload();
+                return;
+            }
+
             gunSound.Play();
             animator.SetBool(AnimFire, true);
             Shoot();
         }
     }
 
-    public void Shoot()
-    {       
+    private void Shoot()
+    {
+        _currentAmmo--;
         if (Physics.Raycast(camera.position, camera.forward, out RaycastHit hit, 100, fireMask, QueryTriggerInteraction.Ignore))
         {
             GameObject impact = Instantiate(impactPrefab, hit.point, Quaternion.identity);
@@ -39,5 +69,19 @@ public class Gun : MonoBehaviour
                 health.ApplyDamage(damage);
             }
         }
+    }
+
+    private void Reload()
+    {
+        StartCoroutine(ReloadRoutine());
+    }
+
+    private IEnumerator ReloadRoutine()
+    {
+        animator.SetBool(AnimReload, true);
+        _reloading = true;
+        yield return new WaitForSeconds(0.5f);
+        _reloading = false;
+        _currentAmmo = clipSize;
     }
 }
